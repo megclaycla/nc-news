8b8e2bf3d5bde52f5f0523e0dc2a487bda20585d
megclaycla <megan.clayton-clarke@outlook.com>
2024-02-23 14:22:27 +0000

    Task 13 in progress

--- listen.js
+++ listen.js
@@ -1,10 +1,15 @@
-function listen(){
-    app.listen(9090, (err) => {
-        if(err){
-            console.log(err)
-        } else {
-            console.log("listening on 9090")
-        }
-    })
-}
-module.exports = listen;
+// function listen(){
+//     app.listen(9090, (err) => {
+//         if(err){
+//             console.log(err)
+//         } else {
+//             console.log("listening on 9090")
+//         }
+//     })
+// }
+// module.exports = listen;
+
+const app = require('./app')
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
